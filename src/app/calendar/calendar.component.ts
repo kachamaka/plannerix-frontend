83847c25ca5ae82a4ef75e5c99b25b89968e0d6a
kachamaka <martin.popov42@gmail.com>
2018-11-16 22:16:41 +0200

    test dates

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  upcommingTestDates = [{"date":new Date(2018,10,26),"subject":"History"},
+  {"date":new Date(2018,10,23),"subject":"History"},
+  {"date":new Date(2018,10,29),"subject":"History"},
+  {"date":new Date(2018,11,5),"subject":"History"},
+  {"date":new Date(2018,11,12),"subject":"History"}];
+
   constructor() { }
 
   ngOnInit() {
+    this.upcommingTestDates.sort((a, b) => {
+      return b.date.getTime() - a.date.getTime();
+    });
   }
 
 }
