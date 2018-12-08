2ba4a764b9f7ac4cb02d0c55dd465bcfb3d6953d
kinghunter58 <traqn02@gmail.com>
2018-12-08 15:19:32 +0200

    script

--- push_to_prod.sh
+++ push_to_prod.sh
@@ -4,7 +4,17 @@ Color='\033[1;36m'
 NC='\033[0m' # NC = no color
 
 if [ $(git rev-parse --abbrev-ref HEAD) == "master" ]; then
-    echo "yey"
+    if [ -z "$(git status --porcelain)" ]; then
+        ng build --prod
+        sleep 10
+        git add ./dist/*
+        git checkout prod
+        git commit -m "auto built"
+        git push origin prod
+        git checkout master
+    else
+        echo "Working directory is not clean" 
+    fi
 else
     echo -e "Should be on branch master. Current branch: ${Color}$(git rev-parse --abbrev-ref HEAD)${NC} !" 
 fi
